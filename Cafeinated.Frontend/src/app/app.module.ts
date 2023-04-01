bd5399cc9abbb211ce1a920fec1e8e85aa8df5a1
romeo <romeo.vasil@icloud.com>
2023-04-01 14:21:13 +0300

    initial Welcome Page

--- Cafeinated.Frontend/src/app/app.module.ts
+++ Cafeinated.Frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeSectionComponent } from './welcome-section/welcome-section.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeSectionComponent
   ],
   imports: [
     BrowserModule,
