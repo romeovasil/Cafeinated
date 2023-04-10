395d8b92fa307fdb62617b4afb7ebc5abee769ea
romeo <romeo.vasil@icloud.com>
2023-04-10 12:41:15 +0300

    added About us page

--- Cafeinated.Frontend/src/app/app.module.ts
+++ Cafeinated.Frontend/src/app/app.module.ts
@@ -3,17 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { WelcomeSectionComponent } from './welcome-section/welcome-section.component';
 
 
+import {RouterModule, Routes} from "@angular/router";
+import {WelcomeSectionComponent} from "./components/welcome-section/welcome-section.component";
+import { AboutUsSectionComponent } from './components/about-us-section/about-us-section.component';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+
+
+
+const routes: Routes = [
+  {path: '', component: WelcomeSectionComponent},
+  {path:'about-us',component: AboutUsSectionComponent}
+  ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    WelcomeSectionComponent
+    WelcomeSectionComponent,
+    AboutUsSectionComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
